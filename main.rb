1cd4d788ce58fd221b0e8a8644fafc9d2217774b
deborahmcvey <30991313-deborahmcvey@users.noreply.replit.com>
2024-01-06 10:14:23 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,12 @@
+puts 'Hello, World!'
+# this is a comment
+puts "Hello World"
+print 'Hello World'
+puts 'Hello\nWorld'
+
+puts "Hello\nWorld"
+print "Hello\nWorld"
+name = "Ruby"
+puts "Hello, #{name}!\nWelcome to programming!"
+name = "Deborah McVey"
+print name

62d9d47cf4651acad857f4c3a14267935afc31ed
deborahmcvey <30991313-deborahmcvey@users.noreply.replit.com>
2024-01-06 10:28:25 +0000

    numbers and boolean
    first practice

--- main.rb
+++ main.rb
@@ -10,3 +10,8 @@ name = "Ruby"
 puts "Hello, #{name}!\nWelcome to programming!"
 name = "Deborah McVey"
 print name
+# integers
+integers = [42, -10, 0]
+# floating-point numbers
+floating_point_numbers = [3.14, -0.001, 2.0]
+# booleans = TrueClass or FalseClass
